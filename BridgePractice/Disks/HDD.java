24356abe5f77c5070a5bacf85290d2ab9e40b186
Max <43775453+JavaAbuser@users.noreply.github.com>
2021-12-26 23:28:02 +0200

    Update HDD.java
    edit HDD.java

--- BridgePractice/Disks/HDD.java
+++ BridgePractice/Disks/HDD.java
@@ -5,13 +5,9 @@ import BridgePractice.Games.Game;
 public class HDD implements Disk{
     private int volume = 200;
     private Game game;
-
-    public int getVolume() {
-        return volume;
-    }
-
+    
     @Override
-    public int getMemoryVolume() {
+    public int getVolume() {
         return volume;
     }
 }
